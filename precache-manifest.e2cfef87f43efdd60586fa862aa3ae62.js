a322b7087b5b0f64529be175b56d6730388029ed
carreterosanti <santiago.carretero@outlook.com>
2020-04-13 17:25:27 -0300

    Updates

---
+++ precache-manifest.e2cfef87f43efdd60586fa862aa3ae62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8768d4833361a10fa6e328d2bedd7b0f",
+    "url": "/React-Konva/index.html"
+  },
+  {
+    "revision": "5d8b1d32fb29d25291a0",
+    "url": "/React-Konva/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2ce05ac6c4b7dfc9ba5",
+    "url": "/React-Konva/static/js/2.0be63b54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Konva/static/js/2.0be63b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8b1d32fb29d25291a0",
+    "url": "/React-Konva/static/js/main.ac0b7551.chunk.js"
+  },
+  {
+    "revision": "6f02be596513ecf3a30d",
+    "url": "/React-Konva/static/js/runtime-main.9d927a17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Konva/static/media/logo.5d5d9eef.svg"
+  }
+]);
